60690bff578007e95b80482f04f6404dd921c7b7
pablolucio97 <pablolucio97@users.noreply.github.com>
2021-09-16 11:59:42 -0300

    Api connection with MirageJS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/GlobalStyle";
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
@@ -19,15 +19,44 @@ function App() {
 
 
   createServer({
-    routes() {
-      this.namespace = 'api'
-      this.get('/transactions', () => {
-        return [
+
+    models:{
+      transaction: Model,
+    },
+
+    seeds(server){
+      server.db.loadData({
+        transactions:[
           {
             id: 1,
-            name: 'Pablo'
-          }
+            title: "Freelance de website",
+            type: "deposit",
+            category: "Dev",
+            amount: 6200,
+            createdAt: "2021-09-16 12:31",
+          },
+          {
+            id: 2,
+            title: "Aluguel",
+            type: "withdraw",
+            category: "Moradia",
+            amount: 1100,
+            createdAt: "2021-09-16 12:31"
+          },
         ]
+      })
+    },
+
+    routes() {
+      this.namespace = 'api'
+
+      this.get('/transactions', () => {
+        return this.schema.all('transaction')
+      })
+
+      this.post('/transactions', (schema, request) => {
+        const data = JSON.parse(request.requestBody)
+        return schema.create('transaction', data)
       })
     }
 
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
+import { useState, FormEvent } from 'react'
 import Modal from 'react-modal'
 
 import { Container, RadioBox, TransactionTypesCoontainer } from './styles'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { useState } from 'react'
+import { api } from '../../services/api'
 
 type NewTransactionModalProps = {
     isOpen: boolean;
@@ -15,7 +16,24 @@ type NewTransactionModalProps = {
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
 
     const [type, setType] = useState('deposit')
+    const [title, setTitle] = useState('')
+    const [value, setValue] = useState(0)
+    const [category, setCategory] = useState('')
 
+    function handleCreateNewTransaction(e: FormEvent) {
+        e.preventDefault()
+
+        const data = {
+            title,
+            value,
+            type,
+            category
+        }
+
+        api.post('/transactions', data)
+
+
+    }
     return (
         <Modal
             isOpen={isOpen}
@@ -29,10 +47,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
             >
                 <img src={closeImg} alt="dt money" />
             </button>
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastrar transação</h2>
-                <input type="text" placeholder="Título" />
-                <input type="number" placeholder="Valor" />
+                <input
+                    type="text"
+                    placeholder="Título"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}
+                />
+                <input
+                    type="number"
+                    placeholder="Valor"
+                    value={value}
+                    onChange={e => setValue(e.target.valueAsNumber)}
+                />
 
                 <TransactionTypesCoontainer>
                     <RadioBox
@@ -55,7 +83,12 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                     </RadioBox>
                 </TransactionTypesCoontainer>
 
-                <input type="text" placeholder="Categoria" />
+                <input
+                    type="text"
+                    placeholder="Categoria"
+                    value={category}
+                    onChange={(e) => setCategory(e.target.value)}
+                />
                 <button type="submit">Cadastrar</button>
             </Container>
         </Modal>
